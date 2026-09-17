194404e567570b081642ef171ed57236cdf7b4e2
Mohd yasir <yasirkhan9554@gmail.com>
2026-09-17 13:58:55 +0530

    Add palindrome program

---
+++ 01_Palindrome.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int n = 121, original = n, reverse = 0;
+        while (n > 0) { reverse = reverse * 10 + n % 10; n /= 10; }
+        System.out.println(original == reverse ? "Palindrome" : "Not Palindrome");
+    }
+}
