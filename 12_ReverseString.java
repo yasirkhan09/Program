e210e1da0bed78bbab9d34c740f7d552f74f38b1
Mohd yasir <yasirkhan9554@gmail.com>
2026-09-17 13:59:55 +0530

    Add reverse string program

---
+++ 12_ReverseString.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String text = "Java";
+        StringBuilder result = new StringBuilder(text).reverse();
+        System.out.println(result);
+    }
+}
