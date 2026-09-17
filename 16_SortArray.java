340ebfccc29470a68f67b686c33118d422402aa7
Mohd yasir <yasirkhan9554@gmail.com>
2026-09-17 14:00:12 +0530

    Add array sorting program

---
+++ 16_SortArray.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] a = {5, 2, 8, 1, 3};
+        Arrays.sort(a);
+        System.out.println(Arrays.toString(a));
+    }
+}
